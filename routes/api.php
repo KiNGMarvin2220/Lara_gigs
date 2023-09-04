da4730d0f631d3a4db38b03a763887ec12caf314
KiNGMarvin2220 <marvindec22galang@gmail.com>
2023-09-04 11:23:56 +0800

    9-4-23 11:18 learing API

--- routes/api.php
+++ routes/api.php
@@ -27,3 +27,6 @@ Route::middleware('auth:sanctum')->get('/user',
 function (Request $request) {
     return $request->user();
 });
+
+// products API
+Route::get('/products', [ProductController::class, 'index']);
